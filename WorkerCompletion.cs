a5b5872f8afebe59650872fefb5c88dda8659749
AnLoffredo <38223101+AnLoffredo@users.noreply.github.com>
2019-08-08 10:20:10 -0700

    Add files via upload

---
+++ WorkerCompletion.cs
@@ -0,0 +1,23 @@
+﻿using Penguin.Entities;
+
+namespace Penguin.Cms.Workers.Entities
+{
+    /// <summary>
+    /// A class representing an instance of a worker completing its work
+    /// </summary>
+    public class WorkerCompletion : AuditableEntity
+    {
+        #region Properties
+        /// <summary>
+        /// Worker completions are immutable and should not need to be audited
+        /// </summary>
+        public override bool AuditLogChanges => false;
+
+        /// <summary>
+        /// The name of the worker this completion is for
+        /// </summary>
+        public string Name { get; set; }
+
+        #endregion Properties
+    }
+}
