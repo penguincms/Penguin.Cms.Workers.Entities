a04272466d453e340c432d1beb10bd59e141b73e
anloffredo <loffredo.ap@gmail.com>
2019-09-15 22:06:06 -0700

    Code Analysis Changes
    Code Analysis Changes

--- WorkerCompletion.cs
+++ WorkerCompletion.cs
@@ -7,7 +7,6 @@ namespace Penguin.Cms.Workers.Entities
     /// </summary>
     public class WorkerCompletion : AuditableEntity
     {
-        #region Properties
         /// <summary>
         /// Worker completions are immutable and should not need to be audited
         /// </summary>
@@ -17,7 +16,5 @@ namespace Penguin.Cms.Workers.Entities
         /// The name of the worker this completion is for
         /// </summary>
         public string Name { get; set; }
-
-        #endregion Properties
     }
 }
