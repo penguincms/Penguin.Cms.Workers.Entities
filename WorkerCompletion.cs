09eeddf54401dbe4ee147f54882f7e7889fa7ab2
anloffredo <loffredo.ap@gmail.com>
2019-10-06 23:16:37 -0700

    Framework Updates
    Framework Updates

--- WorkerCompletion.cs
+++ WorkerCompletion.cs
@@ -1,16 +1,14 @@
-﻿using Penguin.Entities;
+﻿using Penguin.Cms.Auditing.Abstractions;
+using Penguin.Cms.Entities;
 
 namespace Penguin.Cms.Workers.Entities
 {
     /// <summary>
     /// A class representing an instance of a worker completing its work
     /// </summary>
-    public class WorkerCompletion : AuditableEntity
+    [DontAuditChanges]
+    public class WorkerCompletion : Entity
     {
-        /// <summary>
-        /// Worker completions are immutable and should not need to be audited
-        /// </summary>
-        public override bool AuditLogChanges => false;
 
         /// <summary>
         /// The name of the worker this completion is for
